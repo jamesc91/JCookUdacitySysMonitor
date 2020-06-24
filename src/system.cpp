997a34c9090c8cfd8f7817767be93152e75b4ff1
James <jcook@altavian.com>
2020-06-23 21:16:28 -0400

    total processes

--- src/system.cpp
+++ src/system.cpp
@@ -18,7 +18,7 @@ Processor& System::Cpu() { return cpu_; }
 // TODO: Return a container composed of the system's processes
 vector<Process>& System::Processes() { return processes_; }
 
-// TODO: Return the system's kernel identifier (string)
+// Done, define several strings, input file stream from proc/version. I'm lazy so I didn't make proc directory + version directory
 std::string System::Kernel() {   
     string os, kernel;
     string line;
@@ -47,7 +47,7 @@ std::string System::Kernel() {
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
-// TODO: Return the operating system name
+// Done, I need to figure out a way to delete the first space in the double space 
 std::string System::OperatingSystem() { 
     
     string key, value;
@@ -70,7 +70,7 @@ std::string System::OperatingSystem() {
             std::replace(line.begin(), line.end(), equals, space);
             std::replace(line.begin(), line.end(), quote, uscore);
             std::istringstream linestream(line);
-            //std::cout<<"line: "<<line<<std::endl;
+            
             while(linestream >> key >> value){
                 if(key == "PRETTY_NAME"){
                     
@@ -91,7 +91,29 @@ std::string System::OperatingSystem() {
 int System::RunningProcesses() { return 0; }
 
 // TODO: Return the total number of processes on the system
-int System::TotalProcesses() { return 0; }
+int System::TotalProcesses() { 
+    string line;
+    string key, value;
+    int processes;
+    std::ifstream stream(proc_stat_);
+    
+    if(stream.is_open()){
+
+        while(std::getline(stream, line)){
+
+            std::istringstream linestream(line);
+            while(linestream >> key){
+                //std::cout<<"key: "<<key<<std::endl;
+                if(key == "processes"){
+                    linestream >> processes;
+                    return processes;
+                }
+            }
+        }
+    }
+    
+    
+    return 0; }
 
 // TODO: Return the number of seconds since the system started running
 long int System::UpTime() { return 0; }
